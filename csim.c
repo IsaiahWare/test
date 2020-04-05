e5b4698560d56658d118be79aa4db7ef1648da6a
Isaiah Ware <isaiahware99@gmail.com>
2020-04-05 18:26:25 -0500

    yeah

--- csim.c
+++ csim.c
@@ -1,178 +1,242 @@
-/* 
- * trans.c - Matrix transpose B = A^T
- *
- * Each transpose function must have a prototype of the form:
- * void trans(int M, int N, int A[N][M], int B[M][N]);
- *
- * A transpose function is evaluated by counting the number of misses
- * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
 #include "cachelab.h"
 
-int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+// Author: Yue Pan
+// Replacement Policy: LRU
 
-/* 
- * transpose_submit - This is the solution transpose function that you
- *     will be graded on for Part B of the assignment. Do not change
- *     the description string "Transpose submission", as the driver
- *     searches for that string to identify the transpose function to
- *     be graded. 
- */
-char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
-    if (M == 32 && N == 32) {
-        int i, j, k, l, t1, t2, t3, t4, t5, t6, t7, t8;
-        for (i = 0; i < N; i += 8) {
-            for (j = 0; j < M; j += 8) {
-                for (k = i; k < i + 8; k++) {
-                    for (l = j; l < j + 8; l += 8) {
-                        t1 = A[k][l];
-                        t2 = A[k][l + 1];
-                        t3 = A[k][l + 2];
-                        t4 = A[k][l + 3];
-                        t5 = A[k][l + 4];
-                        t6 = A[k][l + 5];
-                        t7 = A[k][l + 6];
-                        t8 = A[k][l + 7];
-                        B[l][k] = t1;
-                        B[l + 1][k] = t2;
-                        B[l + 2][k] = t3;
-                        B[l + 3][k] = t4;
-                        B[l + 4][k] = t5;
-                        B[l + 5][k] = t6;
-                        B[l + 6][k] = t7;
-                        B[l + 7][k] = t8;
-                    }
-                }
-            }
+enum {
+    TYPE_MODIFY,
+    TYPE_LOAD,
+    TYPE_SAVE
+};
+
+enum {
+    HIT,
+    MISS,
+    EVICTION
+};
+
+typedef struct {
+    int visible;
+    int set_num;
+    int entry;
+} cache_size;
+
+typedef struct {
+    unsigned long int valid;
+    unsigned long int tag;
+} cache_line;
+
+typedef struct {
+    char op;
+    unsigned long int addr;
+    unsigned long int index;
+    unsigned long int tag;
+    unsigned int mem_size;
+} operation;
+
+typedef struct {
+    int hits;
+    int misses;
+    int evictions;
+} summary;
+
+void Caching(cache_line *cache, operation an_op, cache_size c_size, summary *my_summary) {
+
+    int index;
+    int type;
+    int result;
+    unsigned long int temp_rag;
+
+    // get type
+    switch (an_op.op) {
+        case 'M':
+            type = TYPE_MODIFY;
+            break;
+        case 'L':
+            type = TYPE_LOAD;
+            break;
+        case 'S':
+            type = TYPE_SAVE;
+            break;
+    }
+    cache_line *pcache = cache + an_op.index * c_size.entry;
+
+    // get result type
+    result = EVICTION;
+    for (index = 0; index < c_size.entry; index++) {
+        if ((pcache + index)->valid == 0) {
+            result = MISS;
+            break;
+        }
+        if ((pcache + index)->tag == an_op.tag) {
+            result = HIT;
+            break;
         }
     }
-    else if (M == 64 && N == 64) {
 
-        int i, j, k, l, t1, t2, t3, t4, t5, t6, t7, t8;
-        for (i = 0; i < N; i += 8) {
-            for (j = 0; j < M; j += 8) {
-                for (k = i; k < i + 4; k++) {
-                    t1 = A[k][j];
-                    t2 = A[k][j + 1];
-                    t3 = A[k][j + 2];
-                    t4 = A[k][j + 3];
-                    t5 = A[k][j + 4];
-                    t6 = A[k][j + 5];
-                    t7 = A[k][j + 6];
-                    t8 = A[k][j + 7];
-
-                    B[j][k] = t1;
-                    B[j + 1][k] = t2;
-                    B[j + 2][k] = t3;
-                    B[j + 3][k] = t4;
-                    B[j][k + 4] = t5;
-                    B[j + 1][k + 4] = t6;
-                    B[j + 2][k + 4] = t7;
-                    B[j + 3][k + 4] = t8;
-                }
-                for (l = j + 4; l < j + 8; l++) {
-
-                    t5 = A[i + 4][l - 4];
-                    t6 = A[i + 5][l - 4];
-                    t7 = A[i + 6][l - 4];
-                    t8 = A[i + 7][l - 4];
-
-                    t1 = B[l - 4][i + 4];
-                    t2 = B[l - 4][i + 5];
-                    t3 = B[l - 4][i + 6];
-                    t4 = B[l - 4][i + 7];
-
-                    B[l - 4][i + 4] = t5;
-                    B[l - 4][i + 5] = t6;
-                    B[l - 4][i + 6] = t7;
-                    B[l - 4][i + 7] = t8;
-
-                    B[l][i] = t1;
-                    B[l][i + 1] = t2;
-                    B[l][i + 2] = t3;
-                    B[l][i + 3] = t4;
-
-                    B[l][i + 4] = A[i + 4][l];
-                    B[l][i + 5] = A[i + 5][l];
-                    B[l][i + 6] = A[i + 6][l];
-                    B[l][i + 7] = A[i + 7][l];
-                }
+    // LRU policy
+    switch (result) {
+        // if HIT, just refrash the time
+        case HIT:
+            temp_rag = (pcache + index)->tag;
+            for ( ; index < c_size.entry - 1 && (pcache + index + 1)->valid == 1; index++) {
+                (pcache + index)->tag = (pcache + index + 1)->tag;
             }
-        }
+            (pcache + index)->tag = temp_rag;
+            my_summary->hits++;
+            break;
+        // if MISS, add it to the cache
+        case MISS:
+            my_summary->misses++;
+            (pcache + index)->valid = 1;
+            (pcache + index)->tag = an_op.tag;
+            break;
+        // if EVICTION, delete the line who have minimum time and add it to the cache
+        case EVICTION:
+            my_summary->misses++;
+            my_summary->evictions++;
+            for (index = 0; index < c_size.entry - 1; index++) {
+                (pcache + index)->tag = (pcache + index + 1)->tag;
+            }
+            (pcache + index)->tag = an_op.tag;
+            break;
     }
-    else if (M == 61 && N == 67) {
-        int i, j, k, l, t;
-        int size = 23;
-        for (i = 0; i < N; i += size) {
-            for (j = 0;  j < M; j += size) {
-                for (k = i; k < i + size && k < N; k++) {
-                    for (l = j; l < j + size && l < M; l++) {
-                        t = A[k][l];
-                        B[l][k] = t;
-                    }
-                }
-            }
+
+    // the write in type modify is sure to hit
+    if (type == TYPE_MODIFY)
+        my_summary->hits++;
+        
+    // print type ( Only visible = 1 )
+    if (c_size.visible == 1) {
+        switch (type) {
+            case TYPE_MODIFY:
+                printf("M ");
+                break;
+            case TYPE_LOAD:
+                printf("L ");
+                break;
+            case TYPE_SAVE:
+                printf("S ");
+                break;
         }
+        printf("%lx,%d ", an_op.addr, an_op.mem_size);
+        switch (result) {
+            case HIT:
+                printf("hit");
+                break;
+            case MISS:
+                printf("miss");
+                break;
+            case EVICTION:
+                printf("miss eviction");
+                break;
+        }
+        if (type == TYPE_MODIFY)
+            printf(" hit");
+        printf("\n");
     }
 }
 
-/* 
- * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
 
-/* 
- * trans - A simple baseline transpose function, not optimized for the cache.
- */
-char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
-
+void print_help_info(void) {
+    // Print the help inforation of this program
+    printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+    printf("Options:\n");
+    printf("  -h         Print this help message.\n");
+    printf("  -v         Optional verbose flag.\n");
+    printf("  -s <num>   Number of set index bits.\n");
+    printf("  -E <num>   Number of lines per set.\n");
+    printf("  -b <num>   Number of block offset bits.\n");
+    printf("  -t <file>  Trace file.\n");
+    printf("\n");
+    printf("Examples:\n");
+    printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+    printf("  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
 }
 
-/*
- * registerFunctions - This function registers your transpose
- *     functions with the driver.  At runtime, the driver will
- *     evaluate each of the registered functions and summarize their
- *     performance. This is a handy way to experiment with different
- *     transpose strategies.
- */
-void registerFunctions()
-{
-    /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+int main(int argc, char **argv) {
 
-    /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
-}
-
-/* 
- * is_transpose - This helper function checks if B is the transpose of
- *     A. You can check the correctness of your transpose by calling
- *     it before returning from the transpose function.
- */
-int is_transpose(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
-                return 0;
-            }
+    // get options
+    char opt;
+    int help = 0, visible = 0, set_power = 0, set_num = 0, entry = 0, block_power = 0, trace = 0;
+    char *filepath;
+    while((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (opt) {
+            case 'h':
+                help = 1;
+                break;
+            case 'v':
+                visible = 1;
+                break;
+            case 's':
+                set_power = atoi(optarg);
+                set_num = 1 << set_power;
+                break;
+            case 'E':
+                entry = atoi(optarg);
+                break;
+            case 'b':
+                block_power = atoi(optarg);
+                break;
+            case 't':
+                trace = 1;
+                filepath = optarg;
+                break;
+            default:
+                help = 1;
+                break;
         }
     }
-    return 1;
+
+    // Print help info
+    if (help == 1) {
+        print_help_info();
+        exit(0);
+    }
+    if (set_power == 0 || entry == 0 || block_power == 0 || trace == 0) {
+        printf("Arguments Error!\n\n");
+        print_help_info();
+        exit(1);
+    }
+
+    // Build cache
+    summary my_summary;
+    cache_size c_size;
+    int index;
+    my_summary.hits = my_summary.misses = my_summary.evictions = 0;
+    c_size.visible = visible, c_size.set_num = set_num, c_size.entry = entry;
+    cache_line *cache = (cache_line *)malloc(sizeof(cache_line) * set_num * entry);
+    if (cache == NULL) {
+        printf("Memory Error!\n");
+        exit(1);
+    }
+    for (index = 0; index < set_num * entry; index++) {
+        (cache + index)->valid = 0;
+        (cache + index)->tag = 0xffffffff;
+    }
+
+    // Read file
+    FILE *trace_file = fopen(filepath, "r");
+    operation an_op;
+    char line[80];
+    char *pline = NULL;
+    while (fgets(line, 80, trace_file) != NULL) {
+        pline = line;
+        if (*pline++ == 'I')
+            continue;
+        sscanf(pline, "%c %lx,%u", &an_op.op, &an_op.addr, &an_op.mem_size);
+        an_op.index = (an_op.addr >> block_power) & ~(~0 << set_power);
+        an_op.tag = an_op.addr >> (block_power + set_power);
+        Caching(cache, an_op, c_size, &my_summary);
+    }
+    free(cache);
+
+    // Print answer
+    printSummary(my_summary.hits, my_summary.misses, my_summary.evictions);
+    return 0;
 }
