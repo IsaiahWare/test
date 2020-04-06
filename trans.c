4dd14bc6cf71de38efa60cf1e49782dadbfa4d79
Isaiah Ware <isaiahware99@gmail.com>
2020-04-05 22:13:59 -0500

    asd

--- trans.c
+++ trans.c
@@ -40,33 +40,19 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]){
  */ 
 
 /*
- * transpose_32 - Matrix transposition for 32x32 matrix
+ * transpose_32 - Matrix transposition function optimized for a 32x32 matrix
  */
 char transpose_32_desc[] = "Transpose a 32x32 matrix";
 void transpose_32(int M, int N, int A[N][M], int B[M][N]){
-  //int i, j, k, l;
-  //int val1, val2, val3, val4, val5, val6, val7, val8;
-  for (int i = 0; i < N; i += 8) {
-      for (int j = 0; j < M; j += 8) {
-          for (int k = i; k < i + 8; k++) {
-              for (int l = j; l < j + 8; l += 8) {
-                  // val1 = A[k][l];
-                  // val2 = A[k][l + 1];
-                  // val3 = A[k][l + 2];
-                  // val4 = A[k][l + 3];
-                  // val5 = A[k][l + 4];
-                  // val6 = A[k][l + 5];
-                  // val7 = A[k][l + 6];
-                  // val8 = A[k][l + 7];
-
-                  B[l][k] = A[k][l];
-                  B[l + 1][k] = A[k][l + 1];
-                  B[l + 2][k] = A[k][l + 2];
-                  B[l + 3][k] = A[k][l + 3];
-                  B[l + 4][k] = A[k][l + 4];
-                  B[l + 5][k] = A[k][l + 5];
-                  B[l + 6][k] = A[k][l + 6];
-                  B[l + 7][k] = A[k][l + 7];
+  int i, j, k, l;
+  int val1, val2, val3, val4, val5, val6, val7, val8;
+  for (i = 0; i < N; i += 8) {
+      for (j = 0; j < M; j += 8) {
+          for (k = i; k < i + 8; k++) {
+              for (l = j; l < j + 8; l += 8) {
+                for (int z = 0; z < 8; z++) {
+                  B[l+z][k] = A[k][l+z]
+                }
               }
           }
       }
@@ -85,21 +71,27 @@ void transpose_64(int M, int N, int A[N][M], int B[M][N]){
               B[j + 1][m] = A[m][j + 1];
               B[j + 2][m] = A[m][j + 2];
               B[j + 3][m] = A[m][j + 3];
+
               B[j][m + 4] = A[m][j + 4];
               B[j + 1][m + 4] = A[m][j + 5];
               B[j + 2][m + 4] = A[m][j + 6];
               B[j + 3][m + 4] = A[m][j + 7];
           }
           for (int n = j + 4; n < j + 8; n++) {
-              int val1 = B[n - 4][i + 4];
-              int val2 = B[n - 4][i + 5];
-              int val3 = B[n - 4][i + 6];
-              int val4 = B[n - 4][i + 7];
+              // int val1 = A[i + 4][n - 4];
+              // int val2 = A[i + 5][n - 4];
+              // int val3 = A[i + 6][n - 4];
+              // int val4 = A[i + 7][n - 4];
+              int val5 = B[n - 4][i + 4];
+              int val6 = B[n - 4][i + 5];
+              int val7 = B[n - 4][i + 6];
+              int val8 = B[n - 4][i + 7];
 
               B[n - 4][i + 4] = A[i + 4][n - 4];
               B[n - 4][i + 5] = A[i + 5][n - 4];
               B[n - 4][i + 6] = A[i + 6][n - 4];
               B[n - 4][i + 7] = A[i + 7][n - 4];
+              
               B[n][i] = val5;
               B[n][i + 1] = val6;
               B[n][i + 2] = val7;
