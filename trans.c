21bc1e6c82bd278afd8aac32b36496bfaa207505
Isaiah Ware <isaiahware99@gmail.com>
2020-04-05 20:45:41 -0500

    asd

--- trans.c
+++ trans.c
@@ -57,27 +57,21 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N]){
  */
 char transpose_32_desc[] = "Transpose a 32x32 matrix";
 void transpose_32(int M, int N, int A[N][M], int B[M][N]){
-  int i, j, k, l, t1, t2, t3, t4, t5, t6, t7, t8;
+  int i, j, k, l, z;
   for (i = 0; i < N; i += 8) {
       for (j = 0; j < M; j += 8) {
           for (k = i; k < i + 8; k++) {
               for (l = j; l < j + 8; l += 8) {
-                  t1 = A[k][l];
-                  t2 = A[k][l + 1];
-                  t3 = A[k][l + 2];
-                  t4 = A[k][l + 3];
-                  t5 = A[k][l + 4];
-                  t6 = A[k][l + 5];
-                  t7 = A[k][l + 6];
-                  t8 = A[k][l + 7];
-                  B[l][k] = t1;
-                  B[l + 1][k] = t2;
-                  B[l + 2][k] = t3;
-                  B[l + 3][k] = t4;
-                  B[l + 4][k] = t5;
-                  B[l + 5][k] = t6;
-                  B[l + 6][k] = t7;
-                  B[l + 7][k] = t8;
+                for (z = 0; z < 8; z++) {
+                  B[l + z][k] = A[k][l];
+                  B[l + z][k] = A[k][l + z];
+                  B[l + z][k] = A[k][l + z];
+                  B[l + z][k] = A[k][l + z];
+                  B[l + z][k] = A[k][l + z];
+                  B[l + z][k] = A[k][l + z];
+                  B[l + z][k] = A[k][l + z];
+                  B[l + z][k] = A[k][l + z];
+                }
               }
           }
       }
